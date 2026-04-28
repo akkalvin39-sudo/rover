548220695e744552d38aef65644d129e45e52bbc
Hello <akkalvin39@gmail.com>
2026-04-28 10:21:50 +0700

    fix(assert): stop motors as well
    Stop the motors when an assert triggers to prevent the robot from
    potentially driving into something at full speed an damaging itself.
    As usual, reduce code dependency by setting the pins directly (avoid
    io.h). The pins are different on the Nsumo and Launchpad, so to make
    it work on both targets regardless, configure them all.
    
    Assert handler is now fully implemented as originally planned:
    1. Stop motors
    2. Breakpoint
    3. Trace
    4. Blink LED
    
    video: 21

--- src/common/assert_handler.c
+++ src/common/assert_handler.c
@@ -4,63 +4,74 @@
 #include "external/printf/printf.h"
 #include <msp430.h>
 
-/* Software breakpoint for MSP430-GCC */
+/* The TI compiler provides intrinsic support for calling a specific opcode, which means
+ * you can write __op_code(0x4343) to trigger a software breakpoint (when LAUNCHPAD FET
+ * debugger is attached). MSP430-GCC does not have this intrinsic, but 0x4343 corresponds
+ * to assembly instruction "CLR.B R3". */
 #define BREAKPOINT __asm volatile("CLR.B R3");
 
-/* Max string size: "ASSERT 0xFFFF\n" */
+// Text + Program counter + Null termination
 #define ASSERT_STRING_MAX_SIZE (15u + 6u + 1u)
 
-/* --- UART TRACE --- */
+#define GPIO_OUTPUT_LOW(port, bit)                                                                 \
+    do {                                                                                           \
+        P##port##SEL &= ~(BIT##bit);                                                               \
+        P##port##SEL2 &= ~(BIT##bit);                                                              \
+        P##port##DIR |= BIT##bit;                                                                  \
+        P##port##REN &= ~(BIT##bit);                                                               \
+        P##port##OUT &= ~(BIT##bit);                                                               \
+    } while (0)
+
 static void assert_trace(uint16_t program_counter)
 {
     uart_init_assert();
-
-    // small delay to stabilize UART
     BUSY_WAIT_ms(10);
-
     char assert_string[ASSERT_STRING_MAX_SIZE];
     snprintf(assert_string, sizeof(assert_string), "ASSERT 0x%x\n", program_counter);
-
     uart_trace_assert(assert_string);
 }
 
-/* --- LED INIT (renamed to match tutorial) */
 static void assert_blink_led(void)
 {
 #if defined(LAUNCHPAD)
-    P1SEL &= ~(BIT0 | BIT6);
-    P1SEL2 &= ~(BIT0 | BIT6);
-    P1DIR |= (BIT0 | BIT6);
-    P1REN &= ~(BIT0 | BIT6);
+    GPIO_OUTPUT_LOW(1, 0);
 #elif defined(NSUMO)
-    P2SEL &= ~(BIT6);
-    P2SEL2 &= ~(BIT6);
-    P2DIR |= (BIT6);
-    P2REN &= ~(BIT6);
+    GPIO_OUTPUT_LOW(2, 6);
 #endif
 }
 
-/* --- MAIN HANDLER --- */
+// Stop the motors when something unexpected happens (assert) to prevent damage
+static void assert_stop_motors(void)
+{
+    GPIO_OUTPUT_LOW(1, 6); // Left PWM (Launchpad)
+    GPIO_OUTPUT_LOW(2, 1); // Left CC1 (Launchpad)
+    GPIO_OUTPUT_LOW(2, 2); // Left CC2 (Launchpad)
+    GPIO_OUTPUT_LOW(2, 4); // Left CC2 (Nsumo)
+    GPIO_OUTPUT_LOW(2, 5); // Left CC1 (Nsumo)
+    GPIO_OUTPUT_LOW(2, 7); // Right CC2 (Nsumo)
+    GPIO_OUTPUT_LOW(3, 7); // Right CC1 (Nsumo)
+    GPIO_OUTPUT_LOW(3, 5); // Left PWM (Nsumo)
+    GPIO_OUTPUT_LOW(3, 6); // Right PWM (Nsumo)
+}
+
+/* Minimize code dependency in this function to reduce the risk of accidently calling
+ * a function with an assert in it, which would cause the assert_handler to be called
+ * recursively until stack overflow. */
 void assert_handler(uint16_t program_counter)
 {
 #if ASSERT_ENABLE_BREAKPOINT
     BREAKPOINT;
 #endif
-
-    assert_blink_led(); // ✅ FIXED
-
+    assert_stop_motors();
+    assert_blink_led();
     while (1) {
-        // keep sending UART (reliable)
         assert_trace(program_counter);
-
-        // blink LED
 #if defined(LAUNCHPAD)
         P1OUT ^= BIT0;
         P1OUT ^= BIT6;
 #elif defined(NSUMO)
         P2OUT ^= BIT6;
 #endif
-
         BUSY_WAIT_ms(500);
     }
 }
